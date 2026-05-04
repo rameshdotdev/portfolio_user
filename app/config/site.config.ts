d862ece2042230b1875ea7fa4f9ab06c45cdd6bd
rameshdotdev <ramesh2041999@gmail.com>
2026-05-04 08:00:58 +0530

    seo desc changes

--- app/config/site.config.ts
+++ app/config/site.config.ts
@@ -3,12 +3,13 @@ import { SiteConfig } from "@/types";
 export const siteConfig: SiteConfig = {
   name: "imramesh.in",
   title:
-    "Ramesh Kumar | Full Stack Developer Portfolio | Next.js & React Developer",
+    "Ramesh Kumar | Full Stack Developer Portfolio | Next.js & React and Node Js Developer",
   description:
-    "Ramesh Kumar is a full stack web developer in India building fast, SEO-friendly Next.js and MERN applications. Hire a freelance developer for portfolio websites, custom SaaS, and responsive React experiences.",
+    "Hey, I'm Ramesh a full stack developer who loves building clean, modern websites and apps where design, functionality, and even the smallest details matter, with a focus on making products that are both practical and visually satisfying.  \n\nTech stack isn't my concern, I'm flexible with whatever the project needs, though I prefer modern frameworks and tools. I'm always open for new opportunities to learn and grow.",
   origin: "https://imramesh.in",
   keywords: [
     "Ramesh Kumar",
+    "rameshdotin",
     "Full Stack Developer Portfolio",
     "MERN Stack Developer",
     "Next.js Developer",
@@ -26,7 +27,7 @@ export const siteConfig: SiteConfig = {
   },
   socials: {
     github: "https://github.com/rameshdotdev",
-    x: "https://x.com/rameshdotdev",
+    x: "https://x.com/rameshdotin",
     linkedin: "https://www.linkedin.com/in/rameshdotin/",
     buymeacoffee: "https://www.buymeacoffee.com/rameshdotin",
   },
